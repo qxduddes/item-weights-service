01a3694a385f3490164ece5000a402ba9757bcd7
Agustin.Qx <agustin@qstrike.com>
2022-06-10 10:58:32 +0800

    feat(pagination): add query filter for item weights

--- controllers/itemWeights.controller.js
+++ controllers/itemWeights.controller.js
@@ -9,7 +9,7 @@ exports.store = (req, res) => {
             return res.status(400).send({ 
                 success: false, 
                 status: 400, 
-                message: err 
+                message: err.message 
             });
         }
 
@@ -51,50 +51,71 @@ exports.get = (req, res) => {
         currentPage = req.query.page;
     }
 
+    const customLabels = {
+        docs: 'data',
+        totalDocs: 'total',
+        limit: 'per_page',
+        page: 'current_page',
+        nextPage: 'next',
+        prevPage: 'prev',
+        totalPages: 'page_count',
+        hasPrevPage: 'has_prev',
+        hasNextPage: 'has_next',
+        pagingCounter: 'page_counter',
+        meta: 'paginator'
+    };
+
+    const options = {
+        page: currentPage,
+        limit: pageLimit,
+        customLabels: customLabels
+    };
+
+    
     let aggregateQuery = ITEM_WEIGHTS.aggregate();
 
     if (req.query.factory && req.query.factory.length > 0) {
         console.log('Filter By Factory: '+ req.query.factory);
-        aggregateQuery = ITEM_WEIGHTS.aggregate([{ $match: { factory: parseInt(req.query.factory) } }]);
+        aggregateQuery = ITEM_WEIGHTS.aggregate([{ $match: { factory: req.query.factory } }]);
     }
 
     if (req.query.brand && req.query.brand.length > 0) {
         console.log('Filter By Brand: '+ req.query.brand);
-        aggregateQuery = ITEM_WEIGHTS.aggregate([{ $match: { brand: parseInt(req.query.brand) } }]);
+        aggregateQuery = ITEM_WEIGHTS.aggregate([{ $match: { brand: req.query.brand } }]);
     }
 
     if (req.query.sport && req.query.sport.length > 0) {
         console.log('Filter By Sport: '+ req.query.sport);
-        aggregateQuery = ITEM_WEIGHTS.aggregate([{ $match: { sport: parseInt(req.query.sport) } }]);
+        aggregateQuery = ITEM_WEIGHTS.aggregate([{ $match: { sport:req.query.sport } }]);
     }
 
     if (req.query.fabric && req.query.fabric.length > 0) {
         console.log('Filter By fabric: '+ req.query.fabric);
-        aggregateQuery = ITEM_WEIGHTS.aggregate([{ $match: { fabric: parseInt(req.query.fabric) } }]);
+        aggregateQuery = ITEM_WEIGHTS.aggregate([{ $match: { fabric: req.query.fabric } }]);
     }
 
     if (req.query.item && req.query.item.length > 0) {
         console.log('Filter By Item: '+ req.query.item);
-        aggregateQuery = ITEM_WEIGHTS.aggregate([{ $match: { item: parseInt(req.query.item) } }]);
+        aggregateQuery = ITEM_WEIGHTS.aggregate([{ $match: { item: req.query.item } }]);
     }
 
     if (req.query.order_number && req.query.order_number.length > 0) {
         console.log('Filter By Order Number: '+ req.query.order_number);
-        aggregateQuery = ITEM_WEIGHTS.aggregate([{ $match: { order_number: parseInt(req.query.order_number) } }]);
+        aggregateQuery = ITEM_WEIGHTS.aggregate([{ $match: { order_number: req.query.order_number } }]);
     }
 
     if (req.query.part_id_number && req.query.part_id_number.length > 0) {
         console.log('Filter By part_id_number: '+ req.query.part_id_number);
-        aggregateQuery = ITEM_WEIGHTS.aggregate([{ $match: { part_id_number: parseInt(req.query.part_id_number) } }]);
+        aggregateQuery = ITEM_WEIGHTS.aggregate([{ $match: { part_id_number: req.query.part_id_number } }]);
     }
 
     // TODO: Find a way to search data inside object
-    // if (req.query.sizes && req.query.sizes.length > 0) {
-    //     console.log('Filter By sizes: '+ req.query.sizes);
-    //     aggregateQuery = ITEM_WEIGHTS.aggregate([{ $match: { weights.sizes: parseInt(req.query.sizes) } }]);
-    // }
+    if (req.query.size && req.query.size.length > 0) {
+        console.log('Filter By sizes: '+ req.query.size);
+        aggregateQuery = ITEM_WEIGHTS.aggregate([{ $match: { 'weights.size': req.query.size } }]);
+    }
     
-    ITEM_WEIGHTS.aggregatePaginate(aggregateQuery, { page: currentPage, limit: pageLimit }, (err, result) => {
+    ITEM_WEIGHTS.aggregatePaginate(aggregateQuery, options, (err, result) => {
         if (err) {
             return res.status(400).send({
                 success: false,
